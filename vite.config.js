45ffc9cd909c10a5aaa09407ab4f81f7d4d454fd
Patrick Kyei <pkyei804@gmail.com>
2023-06-06 16:41:02 +0000

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   css: {
     postcss,
   },
-  base: "/https://github.com/PatrickKyei/skillembassy",
+  base: "/https:/skillembassy",
   plugins: [react()],
   resolve: {
     alias: [
