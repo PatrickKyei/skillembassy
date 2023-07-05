27d680966fd8b494a07a973b8a285a0fdda57226
Patrick Kyei <pkyei804@gmail.com>
2023-07-05 11:23:27 +0000

    add-logos

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -10,14 +10,14 @@ const Hero = () => {
         <div className='flex flex-col text-center gap-[15px] md:w-[600px]'>
             <h1 className='font-bold text-2xl md:text-4xl'>Kickstart Your Lucrative
 Tech Career</h1>
-            <p className='font-semibold text-slate-500 mb-[40px]'>Learn, build and launch an impactful product with your own product designer, product manager,software engineer and cloud engineer.</p>
+            <p className='font-semibold text-slate-500 mb-[40px]'>Learn, build and launch an impactful product with your own product designer, product manager, software engineer and cloud engineer.</p>
         </div>
         <div className='flex justify-center mb-8 md:mb-[100px]'>
           <button
             type='button'
             className='font-bold bg-blue text-sm text-white border-[1px] py-[10px] px-[20px] rounded-full md:text-base hover:drop-shadow-xl'
           >
-            Explore Career Paths
+            Explore Careers
           </button>
           <button
             type='button'
@@ -27,31 +27,31 @@ Tech Career</h1>
           </button>
         </div>
         <div className='flex flex-col gap-6'>
-          <h1 className='font-semibold text-slate-600'>Featured Jobs</h1>
+          <h1 className='font-semibold text-slate-600'>Career Paths</h1>
           <div className='flex flex-col gap-4 lg:flex-row'>
             <Cardhero 
               imgUrl={Figma}
-              jobTitle='UI/UI Developer'
+              jobTitle='Product Design'
               platform='Figma'
-              text='We are looking for an experienced front-end developer to join our team.'
-              tag='Rust'
-              currency='$70,000 - $90,000'
+              text='Learn the step by step method of creating, managing and growing successful products, directly from the leaders who have been there and done that.'
+              tag='Figma'
+              currency='Prototypes'
             />
             <Cardhero 
               imgUrl={Facebook}
-              jobTitle='DataScientist'
-              platform='Facebook'
-              text='We are seeking a data scientist to join our team'
+              jobTitle='Data Science'
+              platform='Big Query'
+              text='Do you want to enjoy the experience of developing and launching a social or human problem from idea to completion with your own designer, product manager, and software developer?'
               tag='Python'
-              currency='$100,000 - $130,000'
+              currency='Data Models'
             />
             <Cardhero 
               imgUrl={Vercel}
-              jobTitle='Technical Writer'
-              platform='Vercel'
-              text='We are seeking a technical writer to join our team.'
-              tag='Documentation'
-              currency='$60,000 - $80,000'
+              jobTitle='DevOps'
+              platform='AWS'
+              text='Learn the step by step method of creating, deploying and automating infrastructures directly from the leaders who have been there and done that.'
+              tag='CI/CD'
+              currency='Automation'
             />
           </div>
         </div>
